12b4353788db4fcf5cbb7b8ee5aad0cb99a9ddaa
Dinholu <alizeehett@hotmail.fr>
2024-01-09 23:32:39 +0100

    add route

--- FRONT/src/app/environments/environments.ts
+++ FRONT/src/app/environments/environments.ts
@@ -4,13 +4,13 @@ export const environment = {
   // backendProduit: '/assets/mock/produits.json',
 
   // prod
-  // backendLoginClient: 'https://projet-hett-alizee.onrender.com/api/utilisateur/login',
-  // backendCatalogue: 'https://projet-hett-alizee.onrender.com/api/catalogue',
-  // backendSignupClient: 'https://projet-hett-alizee.onrender.com/api/utilisateur/signup'
+  backendLoginClient: 'https://projet-hett-alizee.onrender.com/api/utilisateur/login',
+  backendCatalogue: 'https://projet-hett-alizee.onrender.com/api/catalogue',
+  backendSignupClient: 'https://projet-hett-alizee.onrender.com/api/utilisateur/signup'
 
   // local
-  backendLoginClient: 'http://localhost:8080/api/utilisateur/login',
-  backendSignupClient: 'http://localhost:8080/api/utilisateur/signup',
-  backendCatalogue: 'http://localhost:8080/api/catalogue'
+  // backendLoginClient: 'http://localhost:8080/api/utilisateur/login',
+  // backendSignupClient: 'http://localhost:8080/api/utilisateur/signup',
+  // backendCatalogue: 'http://localhost:8080/api/catalogue'
 
 }
